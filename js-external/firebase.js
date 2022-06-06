c6f4e5cb329007be9b9521491868446407daa45b
Rohitvpatil0810 <rohitvpatil0810@gmail.com>
2022-06-06 15:26:30 +0530

    Changing firebase store

--- js-external/firebase.js
+++ js-external/firebase.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBqJHXbl4Rq-KYxoGTT4mRwikBCKe6E9Xo",
-  authDomain: "limerick-db.firebaseapp.com",
-  projectId: "limerick-db",
-  storageBucket: "limerick-db.appspot.com",
-  messagingSenderId: "43009373528",
-  appId: "1:43009373528:web:0e71dcb01a9f807fe3a12d",
+  apiKey: "AIzaSyAu4pJ3eYhFvTjEOqQyjvd-TQwhk0281vA",
+  authDomain: "limerick-db-f3381.firebaseapp.com",
+  projectId: "limerick-db-f3381",
+  storageBucket: "limerick-db-f3381.appspot.com",
+  messagingSenderId: "765075578322",
+  appId: "1:765075578322:web:eecd0f022cee0a6f60bc0b",
+  measurementId: "G-2RKGVM8YGV",
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
